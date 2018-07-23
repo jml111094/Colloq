004c59b97c87edc0589157079404cbd509b92fe0
Julian <julian.luetten@cardioscan.de>
2018-07-23 16:29:20 +0200

    Rev. 1.0.0: Erste Folien der Einführung.

---
+++ LaTeX Beamer FHL CI/00_cleanup.bat
@@ -0,0 +1,17 @@
+del *.synctex
+del *.aux
+del *.blg
+del *.bbl
+del *.log
+del *.synctex
+del *.gz
+del *.nlo
+del *.tcp
+del *.lof
+del *.lot
+del *.toc
+del *.tps
+del *.out
+md .\pdf
+xcopy *.pdf .\pdf /Y
+del *.pdf
